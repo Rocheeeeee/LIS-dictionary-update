124fc0634a4500023f0b88395aae3bf276cf830b
Rocheeeeee <110109183+Rocheeeeee@users.noreply.github.com>
2022-08-05 10:18:09 -0400

    Update dict_update.py

--- dict_update.py
+++ dict_update.py
@@ -10,7 +10,6 @@ st.set_page_config(page_title="Base Dictionary Update", page_icon='random',
 
 
 # load the json file
-@st.cache()
 def load_json(file_name):
     with open(file_name, 'r') as f:
         data = json.load(f)
